d6927696dd5882586040adb6445f895072c1f057
Cameron Clarke <cameronc137@gmail.com>
2018-05-30 20:50:22 -0400

    added a user-friendly default behavior to parser_hallrad.sh which assumes pretty much exactly the configuration structure I am currently using - not portable - doesnt apply to the benchmark configurations unfortunately - assumes user used list_config.txt structure to submit jsub-add jobs to begin with

--- scripts/parser_hallrad.sh
+++ scripts/parser_hallrad.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 if [ $1 == "-h" ]; then
-  echo "usage: ./hallrad_parser ../../prexSim/scripts/list_config.txt"
-else
-  user_list=${1}
+  echo "usage: -m ./parser_hallrad ../../prexSim/scripts/list_config.txt stuff_to_strip_from_front_of_number stuff_..._back_of_number"
+  echo "usage: ./parser_hallrad config"
+elif [ $1 == "-m" ]; then
+  user_list=${2}
   while read p; do
-    #arr=(${p//${2}/ })
-    strippedFront=${p//${2}/}
-    number=${strippedFront//${3}/}
+    strippedFront=${p//${3}/}
+    number=${strippedFront//${4}/}
+    ./ls_${3}.sh $number
+    #echo $number
+  done <$user_list
+else
+  user_list=../../prexSim/scripts/list_${1}.txt
+  while read p; do
+    front="jobs\/prexII_SAMs_cyl_${1}_"
+    strippedFront=${p//$front/}
+    numberm=${strippedFront//'m_900kEv.xml'/}
+    number=$(echo -n $numberm | head -c -1)
     ./ls_${3}.sh $number
     #echo $number
   done <$user_list
