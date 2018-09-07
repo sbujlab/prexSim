88aed00d1cfde960a709cf9633a4114ccb203702
Cameron Clarke <cameronc137@gmail.com>
2018-09-07 18:25:12 -0400

    Fixed new hallRad jlabSubmit code

--- scripts/jlabSubmit_hallrad_new.py
+++ scripts/jlabSubmit_hallrad_new.py
@@ -17,7 +17,6 @@ def main():
     varied = raw_input("Please enter the indicative name: ")#"off_thickness"
     #geo = raw_input("Please enter the can geometry (sph or cyl): ")
     offset = raw_input("Please enter the offset in mm (integers up to 360): ")
-    thin_thickness = raw_input("Please enter the thin thickness of the aluminum can in mils (from 10 to 65): ")
     identifier = "cadSAMs"#raw_input("Please enter the identifier: ")
 
     #sourceDir = "/work/halla/parity/disk1/ciprian/prexSim"
@@ -31,15 +30,15 @@ def main():
 
     if not os.path.exists(outputDir+"/"+jobName+"/log"):
         os.makedirs(outputDir+"/"+jobName+"/log")
-    createXMLfile(sourceDir,outputDir,jobName,identifier,listName,email)
+    createXMLfile(sourceDir,outputDir,jobName,varied,stage,identifier,offset,listName,email)
 
-    call(["cp",sourceDir+"/output/ls_mod.sh",
-              outputDir+"/"+jobName+"/ls_mod.sh"])
+    call(["cp",sourceDir+"/output/ls_mode.sh",
+              outputDir+"/"+jobName+"/ls_mode.sh"])
     call(["cp",sourceMasterDir+"/build/hallRad",
               outputDir+"/"+jobName+"/hallRad"])
     print "All done for configuration ",configuration,"_",identifier,"for",offset
 
-def createXMLfile(sourceDir,outputDir,jobName,identifier,listname,email):
+def createXMLfile(sourceDir,outputDir,jobName,varied,stage,identifier,offset,listName,email):
 
     if not os.path.exists(sourceDir+"/output/jobs"):
         os.makedirs(sourceDir+"/output/jobs")
@@ -49,8 +48,8 @@ def createXMLfile(sourceDir,outputDir,jobName,identifier,listname,email):
     f.write("  <Email email=\""+email+"\" request=\"false\" job=\"true\"/>\n")
     f.write("  <Project name=\"prex\"/>\n")
 
+#    f.write("  <Track name=\"debug\"/>\n")
     f.write("  <Track name=\"analysis\"/>\n")
-#    f.write("  <Track name=\"analysis\"/>\n")
 
     f.write("  <Name name=\""+identifier+"_hallRad\"/>\n")
     f.write("  <OS name=\"centos7\"/>\n")
@@ -58,17 +57,17 @@ def createXMLfile(sourceDir,outputDir,jobName,identifier,listname,email):
 
     f.write("  <Command><![CDATA[\n")
     f.write("    pwd\n")
-    f.write("    ./ls_mode.sh " + varied + " " + identifier + " " + stage + "\n")
+    f.write("    ./ls_mode.sh " + varied + " " + offset + " " + stage + "\n")
     
     f.write("  ]]></Command>\n")
 
     idName= outputDir+"/"+jobName
     f.write("  <Job>\n")
     f.write("    <Input src=\""+idName+"/hallRad\" dest=\"hallRad\"/>\n")
-    f.write("    <Input src=\""+idName+"/ls_mod.sh\" dest=\"ls_mode.sh\"/>\n")
-    #f.write("    <Input src=\""+idName+"/"+listname+".txt\" dest=\""+listname+".txt\"/>\n")
-    f.write("    <Output src=\""+listname+".txt\" dest=\""+idName+"/"+listname+".txt\"/>\n")
-    f.write("    <Output src=\""+listname+"_hallRad.root\" dest=\""+idName+"/"+listname+"_hallRad.root\"/>\n")
+    f.write("    <Input src=\""+idName+"/ls_mode.sh\" dest=\"ls_mode.sh\"/>\n")
+    #f.write("    <Input src=\""+idName+"/"+listName+".txt\" dest=\""+listName+".txt\"/>\n")
+    f.write("    <Output src=\""+listName+".txt\" dest=\""+idName+"/"+listName+".txt\"/>\n")
+    f.write("    <Output src=\""+listName+"_hallRad.root\" dest=\""+idName+"/"+listName+"_hallRad.root\"/>\n")
 
     f.write("    <Stdout dest=\""+idName+"/log/log.out\"/>\n")
     f.write("    <Stderr dest=\""+idName+"/log/log.err\"/>\n")
